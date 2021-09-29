f909e9c632309599b1bb7e884c2bdc038ffca3a1
S.Ignatyuk <signatyuk89@gmail.com>
2021-09-29 17:41:08 +0300

    third commit

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,8 +1,8 @@
 CREATE TABLE trucks
 (
     id int8 generated always as identity,
-    name varchar(25),
-    registration_number varchar (10),
+    name varchar(25) NOT NULL,
+    registration_number varchar (10) NOT NULL,
 
     primary key (id)
 );
@@ -10,10 +10,18 @@ CREATE TABLE trucks
 CREATE TABLE drivers
 (
     id int8 generated always as identity,
-    full_name varchar(30),
+    full_name varchar(30) NOT NULL,
     truck_id int8 UNIQUE,
 
     foreign key (truck_id) references trucks (id),
     primary key (id)
 );
 
+INSERT INTO trucks (name, registration_number) VALUES ('DAF FT XF 105.460', 'AK 0595-1');
+INSERT INTO trucks (name, registration_number) VALUES ('MAN TGX 18.500', 'AM 7171-1');
+INSERT INTO trucks (name, registration_number) VALUES ('Mercedes-Benz Actros 1844', 'AM 3998-1');
+
+INSERT INTO drivers (full_name, truck_id) VALUES ('Иван Иванов', 1);
+INSERT INTO drivers (full_name, truck_id) VALUES ('Василий Васильев', 2);
+INSERT INTO drivers (full_name, truck_id) VALUES ('Андрей Андреев', 3);
+INSERT INTO drivers (full_name, truck_id) VALUES ('Михаил Михайлов', null);
